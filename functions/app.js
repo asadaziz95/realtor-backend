8ae1f08b4e21a4eeea688ca1a2ce015072d1f2b4
Asad Aziz <9146069+asadaziz95@users.noreply.github.com>
2025-02-02 11:28:44 +0500

    fixes

--- functions/app.js
+++ functions/app.js
@@ -1,8 +1,22 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import connectDB from "../config/db.js";
+import userRoutes from "../routes/userRoutes.js";
+import customerRoutes from "../routes/customerRoutes.js";
 const app = express();
 const router = Router();
+app.use(cors());
+app.use(bodyParser.json());
 
+dotenv.config();
+connectDB();
+
+
+app.use("/api/users", userRoutes);
+app.use("/api/customers", customerRoutes);
 router.get("/", (req, res) => {
     res.send("App is running..");
 });
