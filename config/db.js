b2bc3ffec6be77f2759a6e56e78472c88bf1238f
Asad Aziz <9146069+asadaziz95@users.noreply.github.com>
2025-02-09 00:03:31 +0500

    fixes in db.js file

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ console.log('process.env.MONGO_URI',process.env.MONGO_URI);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect('mongodb+srv://asadazizjs:ZEZjz8EhJVMsbSv@cluster0.9fkd1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
